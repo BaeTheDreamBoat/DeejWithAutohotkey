d81d9dd9e6b04c462d6a53d17f878633878236fb
Ryan <rb91827@gmail.com>
2022-08-25 21:25:51 +0000

    hello

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
  * 2022/8/23
  *
  * A simple hello world program in C
- *
+ *My token: https://BaeTheDreamBoat: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@github.com/BaeTheDreamBoat/hello.git
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
 
   printf("Hello World!\n");
   printf("Ryan Brown\n");
-  printf("My major is electical Engineering :)\n");
+  printf("My major is Electical Engineering :)\n");
 
   return 0;
 }
