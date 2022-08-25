36c55db28de085485276337fe3acafe08b177303
Ryan <rb91827@gmail.com>
2022-08-25 20:21:51 +0000

    added major to output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Ryan Brown
+ * rbrown69@unl.edu
+ * 2022/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Ryan Brown\n");
+  printf("My major is electical Engineering :)\n");
+
+  return 0;
+}
